8c762e678a958362c95b6058f9e79c66545ab3a9
Caio Henrique <caiohds2020@gmail.com>
2023-01-01 16:53:29 -0300

    implementando descontos

--- CursoDeDesignPatterns/CursoDeDesignPatterns/Program.cs
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Program.cs
@@ -4,15 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Imposto iss = new ISS();
-        Imposto icms = new ICMS();
-        Imposto iccc = new ICCC();
+        DescontoPorMaisDe5Itens desconto1 = new DescontoPorMaisDe5Itens();
+        DescontoPorValorMaiorQue500 desconto2 = new DescontoPorValorMaiorQue500();
+        SemDesconto desconto3 = new SemDesconto();
 
-        Orcamento orcamento = new Orcamento(4000.0);
+        desconto1.Proximo = desconto2;
+        desconto2.Proximo = desconto3;
 
-        CalculadorDeImposto calculador = new CalculadorDeImposto();
+        Orcamento orcamento = new Orcamento(600);
+        Item item1 = new Item("item1", 50);
+        Item item2 = new Item("item2", 50);
+        Item item3 = new Item("item3", 50);
+        Item item4 = new Item("item4", 50);
+        Item item5 = new Item("item5", 50);
+        Item item6 = new Item("item6", 50);
 
-        calculador.RealizaCalculo(orcamento, iccc);
+        orcamento.adicionaItem(item1);
+        orcamento.adicionaItem(item2);
+        orcamento.adicionaItem(item3);
+        orcamento.adicionaItem(item4);
+        orcamento.adicionaItem(item5);
+
+        Console.WriteLine(desconto1.Desconta(orcamento).ToString("C"));
     }
 }
 
