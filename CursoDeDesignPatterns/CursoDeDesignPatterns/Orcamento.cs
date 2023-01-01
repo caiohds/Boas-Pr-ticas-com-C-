80a5bc38efe320d6dde61fae87afc97eaf4b6be2
Caio Henrique <caiohds2020@gmail.com>
2023-01-01 16:35:42 -0300

    Implementando lista de itens na classe orcamento

--- CursoDeDesignPatterns/CursoDeDesignPatterns/Orcamento.cs
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Orcamento.cs
@@ -9,10 +9,15 @@ namespace CursoDeDesignPatterns
     public class Orcamento
     {
         public double  Valor { get; set; }
-
+        public IList<Item> Itens;
         public Orcamento(double valor)
         {
             Valor = valor;
+            Itens = new List<Item>();
+        }
+        public void adicionaItem(Item item)
+        {
+            Itens.Add(item);
         }
     }
 }
