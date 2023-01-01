de5655d198a1e5c48675bdc88c5f5fd03eb2e404
Caio Henrique <caiohds2020@gmail.com>
2023-01-01 19:03:08 -0300

    Criação da classe 'TemplateImpostoCondicional'

---
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Impostos/TemplateImpostoCondicional.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDeDesignPatterns.Impostos
+{
+    public abstract class TemplateImpostoCondicional : Imposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (DeveUsarTaxacaoMaxima(orcamento))
+            {
+                return MaximaTaxacao(orcamento);
+            }
+            return MinimaTaxacao(orcamento);
+        }
+        public abstract bool DeveUsarTaxacaoMaxima(Orcamento orcamento);
+        public abstract double MaximaTaxacao(Orcamento orcamento);
+        public abstract double MinimaTaxacao(Orcamento orcamento);
+    }
+}
