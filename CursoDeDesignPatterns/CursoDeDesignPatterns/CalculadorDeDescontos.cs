48b440dbc02d48395593e585725a0f10d7e5e9b7
Caio Henrique <caiohds2020@gmail.com>
2023-01-01 17:15:10 -0300

    Criação da classe 'CalculadorDeDescontos'

---
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/CalculadorDeDescontos.cs
@@ -0,0 +1,27 @@
+﻿using CursoDeDesignPatterns.Descontos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDeDesignPatterns
+{
+    public class CalculadorDeDescontos
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            DescontoPorMaisDe5Itens desconto1 = new DescontoPorMaisDe5Itens();
+            DescontoPorValorMaiorQue500 desconto2 = new DescontoPorValorMaiorQue500();
+            DescontoParaCanetaOuLapis desconto3 = new DescontoParaCanetaOuLapis();
+            SemDesconto desconto4 = new SemDesconto();
+
+
+            desconto1.Proximo = desconto2;
+            desconto2.Proximo = desconto3;
+            desconto3.Proximo = desconto4;
+
+            return desconto1.Desconta(orcamento);
+        }
+    }
+}
--- CursoDeDesignPatterns/CursoDeDesignPatterns/Program.cs
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Program.cs
@@ -5,31 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        DescontoPorMaisDe5Itens desconto1 = new DescontoPorMaisDe5Itens();
-        DescontoPorValorMaiorQue500 desconto2 = new DescontoPorValorMaiorQue500();
-        DescontoParaCanetaOuLapis desconto3 = new DescontoParaCanetaOuLapis();
-        SemDesconto desconto4 = new SemDesconto();
-        
-
-        desconto1.Proximo = desconto2;
-        desconto2.Proximo = desconto3;
-        desconto3.Proximo = desconto4;
+       CalculadorDeDescontos calculador = new CalculadorDeDescontos();
 
         Orcamento orcamento = new Orcamento(500);
-        Item item1 = new Item("Caneta", 50);
-        Item item2 = new Item("item2", 50);
-        Item item3 = new Item("item3", 50);
-        Item item4 = new Item("item4", 50);
-        Item item5 = new Item("item5", 50);
-        Item item6 = new Item("item6", 50);
 
-        orcamento.adicionaItem(item1);
-        orcamento.adicionaItem(item2);
-        orcamento.adicionaItem(item3);
-        orcamento.adicionaItem(item4);
-        orcamento.adicionaItem(item5);
+        orcamento.adicionaItem(new Item("Caneta", 50));
+        orcamento.adicionaItem(new Item("item2", 50));
+        orcamento.adicionaItem(new Item("item3", 50));
+        orcamento.adicionaItem(new Item("item4", 50));
+        orcamento.adicionaItem(new Item("item5", 50));
 
-        Console.WriteLine(desconto1.Desconta(orcamento).ToString("C"));
+        Console.WriteLine(calculador.Calcula(orcamento).ToString("C"));
     }
 }
 
