a7fc1667db829e557f877429f44fad776ca7f9ac
Caio Henrique <caiohds2020@gmail.com>
2023-01-01 19:06:14 -0300

    criação do imposto 'ICPP'

---
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Impostos/ICPP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDeDesignPatterns.Impostos
+{
+    public class ICPP : TemplateImpostoCondicional
+    {
+        public override bool DeveUsarTaxacaoMaxima(Orcamento orcamento)
+        {
+            if(orcamento.Valor >= 500)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.07;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.05;
+        }
+    }
+}
