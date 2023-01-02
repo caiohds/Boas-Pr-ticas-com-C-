797f498d8acd337ed5935f829b0bc1d460aeefe2
Caio Henrique <caiohds2020@gmail.com>
2023-01-02 15:44:03 -0300

    ISS atualizado

--- CursoDeDesignPatterns/CursoDeDesignPatterns/Impostos/ISS.cs
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Impostos/ISS.cs
@@ -9,9 +9,12 @@ namespace CursoDeDesignPatterns.Impostos
 {
     public class ISS : Imposto
     {
-        public double Calcula(Orcamento orcamento)
+        public ISS(): base() { }
+        public ISS(Imposto proximoimposto): base(proximoimposto) { }
+        
+        public override double Calcula(Orcamento orcamento)
         {
-            return orcamento.Valor * 0.06;
+            return orcamento.Valor * 0.06 + CalculaProximoImposto(orcamento);
         }
     }
 }
