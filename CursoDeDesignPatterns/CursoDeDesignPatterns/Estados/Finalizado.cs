4f327958f29a1c39b2219834a77956ac5b47558e
Caio Henrique <caiohds2020@gmail.com>
2023-01-02 17:38:32 -0300

    Criação do estado 'Finalizado'

---
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Estados/Finalizado.cs
@@ -0,0 +1,32 @@
+﻿using CursoDeDesignPatterns.Auxiliares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDeDesignPatterns.Estados
+{
+    public class Finalizado : EstadoOrcamento
+    {
+        public void AplicaDescontoExtra(Orcamento orcamento)
+        {
+            throw new Exception("Ñão é possível aplicar desconto para um orçamento que já foi finalizado!");
+        }
+
+        public void Aprova(Orcamento orcamento)
+        {
+            throw new Exception("O orçamento já foi finalizado!");
+        }
+
+        public void Finaliza(Orcamento orcamento)
+        {
+            throw new Exception("O orçamento já foi finalizado!");
+        }
+
+        public void Reprova(Orcamento orcamento)
+        {
+            throw new Exception("O orçamento já foi finalizado!");
+        }
+    }
+}
