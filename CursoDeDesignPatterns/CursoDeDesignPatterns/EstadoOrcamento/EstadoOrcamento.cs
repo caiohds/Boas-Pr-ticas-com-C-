a3c6013ac664dfbd86e879de96249415bf4fe2fa
Caio Henrique <caiohds2020@gmail.com>
2023-01-02 17:21:34 -0300

    Criação da classe 'EstadoOrcamento'

---
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/EstadoOrcamento/EstadoOrcamento.cs
@@ -0,0 +1,17 @@
+﻿using CursoDeDesignPatterns.Auxiliares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDeDesignPatterns.EstadoOrcamento
+{
+    public interface EstadoOrcamento
+    {
+        void AplicaDescontoExtra(Orcamento orcamento);
+        void Aprova(Orcamento orcamento);
+        void Reprova(Orcamento orcamento);
+        void Finaliza(Orcamento orcamento);
+    }
+}
