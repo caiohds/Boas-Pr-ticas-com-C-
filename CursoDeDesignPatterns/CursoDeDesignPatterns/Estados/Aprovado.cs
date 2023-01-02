f07ef1e227ab50e702062e1bb62735420f551a6e
Caio Henrique <caiohds2020@gmail.com>
2023-01-02 17:36:34 -0300

    Criação do estado 'Aprovado'

---
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/Estados/Aprovado.cs
@@ -0,0 +1,32 @@
+﻿using CursoDeDesignPatterns.Auxiliares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDeDesignPatterns.Estados
+{
+    public class Aprovado : EstadoOrcamento
+    {
+        public void AplicaDescontoExtra(Orcamento orcamento)
+        {
+            orcamento.Valor -= orcamento.Valor * 0.02;
+        }
+
+        public void Aprova(Orcamento orcamento)
+        {
+            throw new Exception("O orcamento já está aprovado");
+        }
+
+        public void Finaliza(Orcamento orcamento)
+        {
+            orcamento.Estado = new Finalizado();
+        }
+
+        public void Reprova(Orcamento orcamento)
+        {
+            throw new Exception("O orcamento ja foi aprovado, não é possível reprovar");
+        }
+    }
+}
