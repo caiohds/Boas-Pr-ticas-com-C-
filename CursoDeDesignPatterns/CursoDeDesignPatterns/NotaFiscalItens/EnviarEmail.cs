0a05a02839de7b973a1a63d75b332dadb8b39707
Caio Henrique <caiohds2020@gmail.com>
2023-01-03 09:11:25 -0300

    Criação da classe 'EnviarEmail'

---
+++ CursoDeDesignPatterns/CursoDeDesignPatterns/NotaFiscalItens/EnviarEmail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDeDesignPatterns.NotaFiscalItens
+{
+    public class EnviarEmail : AcaoAposGerarNota
+    {
+        public void Exibir(NotaFiscal nota)
+        {
+            Console.WriteLine("EmailEnviado");
+        }
+    }
+}
