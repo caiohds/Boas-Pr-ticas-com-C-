ad044dc74a2682a47acbcbc5509c808613eeaa4b
Caio Henrique <caiohds2020@gmail.com>
2023-01-04 11:51:38 -0300

    utilizando factory

---
+++ DesignPatterns2/DesignPatterns2/Capitulo1/ConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Capitulo1
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "User Id=root;Password=root;Server=localhost;Database=meuBanco";
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
--- DesignPatterns2/DesignPatterns2/Program.cs
+++ DesignPatterns2/DesignPatterns2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPatterns2.Capitulo1;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -7,6 +8,6 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        IDbConnection conexao = new ConnectionFactory().GetConnection();
     }
 }
