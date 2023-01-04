04b0d53d04b8be9639132b40aab8fe99bc3984af
Caio Henrique <caiohds2020@gmail.com>
2023-01-04 12:03:32 -0300

    Criação da classe 'NotasMusicais'

---
+++ DesignPatterns2/DesignPatterns2/Capitulo2/NotasMusicais.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Capitulo2
+{
+    public class NotasMusicais
+    {
+        private static IDictionary<string,INota> notas = new Dictionary<string, INota>()
+        {
+            {"do", new Do() },
+            {"Re", new Re() },
+            {"Mi", new Mi() },
+            {"Fa", new Fa() },
+            {"Sol", new Sol() },
+            {"La", new La()},
+            { "Si", new Si() }
+        };
+    }
+}
