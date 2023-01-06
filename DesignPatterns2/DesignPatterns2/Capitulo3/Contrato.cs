ff35cbe11c8ad6aae48e490d5a6e35fbc2dc6b2c
Caio Henrique <caiohds2020@gmail.com>
2023-01-06 11:40:01 -0300

    Criação da classe 'Contrato'

---
+++ DesignPatterns2/DesignPatterns2/Capitulo3/Contrato.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Capitulo3
+{
+    public class Contrato
+    {
+        public DateTime Data { get; private set; }
+        public string Cliente { get; private set; }
+        public TipoContrato Tipo { get; private set; }
+
+        public Contrato(DateTime data, String cliente, TipoContrato tipo)
+        {
+            this.Data = data;
+            this.Cliente = cliente;
+            this.Tipo = tipo;
+        }
+
+        public void Avanca()
+        {
+            if (this.Tipo == TipoContrato.Novo) this.Tipo = TipoContrato.EmAndamento;
+            else if (this.Tipo == TipoContrato.EmAndamento) this.Tipo = TipoContrato.Acertado;
+            else if (this.Tipo == TipoContrato.Acertado) this.Tipo = TipoContrato.Concluido;
+        }
+
+    }
+}
